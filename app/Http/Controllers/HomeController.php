7c9b393aec2d6c50f456b3bda3505e47fb2e702a
Krzysztof Stolc <stolckrzysztof711@gmail.com>
2023-10-07 21:05:19 +0200

    pierwsza apka w laravel

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index() {
+        return 'Pierwsza strona';
+    }
+
+    public function show() {
+        $array = [
+            1,2,3,4,'a',5
+        ];
+        dd($array);
+        return 'tests';
+    }
+}
+
